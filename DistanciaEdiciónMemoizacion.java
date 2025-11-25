91601b375d4a58754e6ac229c25f2f109cf5dfe8
ReginaCortes <146691181+reginacortes@users.noreply.github.com>
2025-11-25 16:32:34 -0600

    Add file reading and character counting methods

--- DistanciaEdiciónMemoizacion.java
+++ DistanciaEdiciónMemoizacion.java
@@ -83,7 +83,112 @@ public class DistanciaEdiciónMemoizacion {
         }
         return memo[i][j];
     }
+
+    public static int getNumCaracteres(String s){
+        return s.length();
+    }
+
+    public static String leerArchivo(String rutaArchivo) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(rutaArchivo))).trim();
+    }
     
+    public static void main(String[] args){
+        try{
+            // longitud 5
+            String s1 = leerArchivo("/Users/reginacortes/NetBeansProjects/EDA/cadena1-5.txt");
+            String s2 = leerArchivo("/Users/reginacortes/NetBeansProjects/EDA/cadena2-5.txt");
+
+            //System.out.println("Cadena 1 - 5: " + s1);
+            //System.out.println("Cadena 2 - 5: " + s2);
+
+            System.out.println("Numero de caracteres cadena 1 - longitud 5:" + getNumCaracteres(s1));
+            System.out.println("Numero de caracteres cadena 2 - longitud 5:" + getNumCaracteres(s2));
+
+            //medimos tiempo de ejecucion
+            long inicio = System.nanoTime();
+
+            int distancia = memoizacion(s1, s2);
+
+            long fin = System.nanoTime();
+
+            long duracion = fin - inicio; // en nanosegundos
+
+            System.out.println("\nLa distancia entre las cadenas es: " + distancia);
+            System.out.println("\nTiempo de ejecucion (nanosegundos): " + duracion);
+
+            //longitud 50
+            String s3 = leerArchivo("/Users/reginacortes/NetBeansProjects/EDA/cadena1-50.txt");
+            String s4 = leerArchivo("/Users/reginacortes/NetBeansProjects/EDA/cadena2-50.txt");
+
+            //System.out.println("Cadena 1 - 50: " + s3);
+            //System.out.println("Cadena 2 - 50: " + s4);
+
+            System.out.println("Numero de caracteres cadena 1 - longitud 50:" + getNumCaracteres(s3));
+            System.out.println("Numero de caracteres cadena 2 - longitud 50:" + getNumCaracteres(s4));
+
+            //medimos tiempo de ejecucion
+            long inicio2 = System.nanoTime();
+
+            int distancia2 = memoizacion(s3, s4);
+
+            long fin2 = System.nanoTime();
+
+            long duracion2 = fin2 - inicio2; // en nanosegundos
+
+            System.out.println("\nLa distancia entre las cadenas es: " + distancia2);
+            System.out.println("\nTiempo de ejecucion (nanosegundos): " + duracion2);
+
+            //longitud 500
+
+            String s5 = leerArchivo("/Users/reginacortes/NetBeansProjects/EDA/cadena1-500.txt");
+            String s6 = leerArchivo("/Users/reginacortes/NetBeansProjects/EDA/cadena2-500.txt");
+
+            //System.out.println("Cadena 1 - 500: " + s5);
+            //System.out.println("Cadena 2 - 500: " + s6);
+
+            System.out.println("Numero de caracteres cadena 1 - longitud 500:" + getNumCaracteres(s5));
+            System.out.println("Numero de caracteres cadena 2 - longitud 500:" + getNumCaracteres(s6));
+
+            //medimos tiempo de ejecucion
+            long inicio3 = System.nanoTime();
+
+            int distancia3 = memoizacion(s5, s6);
+
+            long fin3 = System.nanoTime();
+
+            long duracion3 = fin3 - inicio3; // en nanosegundos
+
+            System.out.println("\nLa distancia entre las cadenas es: " + distancia3);
+            System.out.println("\nTiempo de ejecucion (nanosegundos): " + duracion3);
+
+            //longitud 5000
+            String s7 = leerArchivo("/Users/reginacortes/NetBeansProjects/EDA/cadena1-5000.txt");
+            String s8 = leerArchivo("/Users/reginacortes/NetBeansProjects/EDA/cadena2-5000.txt");
+
+            //System.out.println("Cadena 1 - 5000: " + s7);
+            //System.out.println("Cadena 2 - 5000: " + s8);
+
+            System.out.println("Numero de caracteres cadena 1 - longitud 5000:" + getNumCaracteres(s7));
+            System.out.println("Numero de caracteres cadena 2 - longitud 5000:" + getNumCaracteres(s8));
+
+            //medimos tiempo de ejecucion
+            long inicio4 = System.nanoTime();
+
+            int distancia4 = memoizacion(s7, s8);   
+
+            long fin4 = System.nanoTime();
+
+            long duracion4 = fin4 - inicio4; // en nanosegundos
+
+            System.out.println("\nLa distancia entre las cadenas es: " + distancia4);
+            System.out.println("\nTiempo de ejecucion (nanosegundos): " + duracion4);
+
+
+        } catch (IOException e) {
+            System.err.println("Error al leer los archivos: " + e.getMessage());
+
+        }
+    }
     
     
 }
